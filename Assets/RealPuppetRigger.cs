c7601cacfc3c221bed4a7528ef2165f73b73b225
Ryan Nelson <hello@ryannelson.me>
2019-03-29 01:59:27 -0700

    Added Rigged Worm
    - Very rough pass at rigging

---
+++ Assets/RealPuppetRigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Thinko
+{
+
+    [RequireComponent(typeof(RealPuppet))]
+    public class RealPuppetRigger : MonoBehaviour
+    {
+
+        RealPuppet puppet;
+
+        void Start()
+        {
+
+            puppet = GetComponent<RealPuppet>();
+        }
+
+        public void StartRiggingPuppet()
+        {
+            StartCoroutine(RigPuppet());
+
+        }
+
+        private IEnumerator RigPuppet()
+        {
+        
+            yield return new WaitForEndOfFrame();
+
+
+        }
+
+    }
+}
