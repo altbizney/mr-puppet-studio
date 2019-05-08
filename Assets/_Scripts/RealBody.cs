f8f9aae12773c44c4df3cd0b0b9bc69af3a43b96
Luís Fonseca <luispedrofonseca@gmail.com>
2019-05-08 23:02:06 +0100

    RealBody setup

---
+++ Assets/_Scripts/RealBody.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Thinko
+{
+    public class RealBody : MonoBehaviour
+    {
+        public bool SetLocalRotation;
+        public List<RealPuppet.PuppetJoint> PuppetJoints = new List<RealPuppet.PuppetJoint>();
+
+        private void Update()
+        {
+            foreach (var puppetJoint in PuppetJoints)
+            {
+                if (!puppetJoint.Enabled || puppetJoint.RealPuppetDataProvider == null || puppetJoint.Joint == null) continue;
+
+                if (SetLocalRotation)
+                {
+                    puppetJoint.Joint.localRotation = Quaternion.Slerp(
+                        puppetJoint.Joint.localRotation, 
+                        Quaternion.Inverse(puppetJoint.Joint.parent.localRotation) * puppetJoint.RealPuppetDataProvider.GetInput(puppetJoint.InputSource) * Quaternion.Euler(puppetJoint.Offset), 
+                        puppetJoint.Sharpness);
+                }
+                else
+                {
+                    puppetJoint.Joint.rotation = Quaternion.Slerp(
+                        puppetJoint.Joint.rotation, 
+                        puppetJoint.RealPuppetDataProvider.GetInput(puppetJoint.InputSource) * Quaternion.Euler(puppetJoint.Offset),// * Quaternion.Inverse(puppetJoint.TPose), 
+                        puppetJoint.Sharpness);
+                }
+            }
+        }
+    }
+}
