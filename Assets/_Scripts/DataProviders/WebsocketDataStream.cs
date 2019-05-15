f9246a29ed4b71228523defa7726c92fdb85c14d
Jacob Bijani <jacob.bijani@gmail.com>
2019-05-15 19:04:31 -0400

    stash some commented out code and vscode ran prettier

--- Assets/_Scripts/DataProviders/WebsocketDataStream.cs
+++ Assets/_Scripts/DataProviders/WebsocketDataStream.cs
@@ -33,7 +33,8 @@ namespace Thinko
             StartCoroutine(GrabData());
         }
 
-        private void Update(){
+        private void Update()
+        {
             if (Input.GetKeyDown("space"))
             {
                 WristRotation_calib = WristRotation_curr;
@@ -78,16 +79,19 @@ namespace Thinko
                         _shoulder = _array[3].Split(',');
 
                         // initialize raw quaternions
-                        WristRotation_curr = new Quaternion(float.Parse(_wrist[0]),float.Parse(_wrist[1]),float.Parse(_wrist[2]),float.Parse(_wrist[3]));
-                        ElbowRotation_curr = new Quaternion(float.Parse(_elbow[0]),float.Parse(_elbow[1]),float.Parse(_elbow[2]),float.Parse(_elbow[3]));
-                        ShoulderRotation_curr = new Quaternion(float.Parse(_shoulder[0]),float.Parse(_shoulder[1]),float.Parse(_shoulder[2]),float.Parse(_shoulder[3]));
-                        
+                        WristRotation_curr = new Quaternion(float.Parse(_wrist[0]), float.Parse(_wrist[1]), float.Parse(_wrist[2]), float.Parse(_wrist[3]));
+                        ElbowRotation_curr = new Quaternion(float.Parse(_elbow[0]), float.Parse(_elbow[1]), float.Parse(_elbow[2]), float.Parse(_elbow[3]));
+                        ShoulderRotation_curr = new Quaternion(float.Parse(_shoulder[0]), float.Parse(_shoulder[1]), float.Parse(_shoulder[2]), float.Parse(_shoulder[3]));
+
                         // grab calibration info
                         WristCalibrationData.Set(int.Parse(_wrist[4]), int.Parse(_wrist[5]), int.Parse(_wrist[6]), int.Parse(_wrist[7]));
                         ElbowCalibrationData.Set(int.Parse(_elbow[4]), int.Parse(_elbow[5]), int.Parse(_elbow[6]), int.Parse(_elbow[7]));
                         ShoulderCalibrationData.Set(int.Parse(_shoulder[4]), int.Parse(_shoulder[5]), int.Parse(_shoulder[6]), int.Parse(_shoulder[7]));
 
                         // apply calibration
+                        // WristRotation = WristRotation_curr;
+                        // ElbowRotation = ElbowRotation_curr;
+                        // ShoulderRotation = ShoulderRotation_curr;
                         WristRotation = WristRotation_curr * Quaternion.Inverse(WristRotation_calib);
                         ElbowRotation = ElbowRotation_curr * Quaternion.Inverse(ElbowRotation_calib);
                         ShoulderRotation = ShoulderRotation_curr * Quaternion.Inverse(ShoulderRotation_calib);
