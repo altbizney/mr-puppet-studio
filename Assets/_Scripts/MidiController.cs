c62846d1c9e84e9866683d3f9275f4a5b4049195
Jacob Bijani <jacob.bijani@gmail.com>
2019-06-20 17:53:45 -0400

    wip

---
+++ Assets/_Scripts/MidiController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using MidiJack;
+
+public class MidiController : MonoBehaviour
+{
+    // 3 9 12 13
+    public int knobNumber = 3;
+    public SkinnedMeshRenderer skinnedRenderer;
+    public int blendShape = 0;
+
+    [ReadOnly]
+    public float knobPercent;
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        knobPercent = MidiMaster.GetKnob(knobNumber);
+        skinnedRenderer.SetBlendShapeWeight(blendShape, Mathf.Lerp(0f, 100f, knobPercent));
+    }
+}
