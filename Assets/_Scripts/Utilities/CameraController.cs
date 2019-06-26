fb2e56c021ecc2f53f396e881b9481b750c0254e
Luis Fonseca <luispedrofonseca@gmail.com>
2019-06-26 11:11:02 +0200

    Added the camera controller so we can easily follow the puppet - Press 'C' to center on it

---
+++ Assets/_Scripts/Utilities/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using Thinko;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float Distance = 5;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            var realBody = FindObjectOfType<RealBody>();
+
+            transform.position = realBody.WristJoint.position - realBody.WristJoint.forward * Distance;
+            transform.LookAt(realBody.WristJoint.position);
+        }
+    }
+}
