7fccdf7b213c4ae234886d61c7b51fd847d98e7e
Jacob Bijani <jacob.bijani@gmail.com>
2019-07-02 17:12:39 -0400

    start on dummy

---
+++ Assets/_Scripts/Dummy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Thinko
+{
+    public class Dummy : MonoBehaviour
+    {
+        [Required]
+        public RealPuppetDataProvider DataProvider;
+
+        public Transform ShoulderJoint;
+        public Transform ElbowJoint;
+        public Transform WristJoint;
+        public Transform JawTop;
+        public Transform JawBottom;
+
+        private void Update()
+        {
+            // Rotate the joints
+            ShoulderJoint.localRotation = DataProvider.GetInput(RealPuppetDataProvider.Source.Shoulder);
+            ElbowJoint.localRotation = DataProvider.GetInput(RealPuppetDataProvider.Source.Elbow);
+            WristJoint.rotation = DataProvider.GetInput(RealPuppetDataProvider.Source.Wrist);
+
+            // TODO: apply tpose
+        }
+    }
+}
