46c281a0106491276c2ebdb9cbcd21d86d6793ee
Jacob Bijani <jacob.bijani@gmail.com>
2019-07-07 13:46:01 -0400

    wip key remapper

---
+++ Assets/_Scripts/DrivenKeyRemapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Thinko.MrPuppet
+{
+    public class DrivenKeyRemapper : MonoBehaviour
+    {
+        [Serializable]
+        public class Key {
+            public float inputMin = 0f;
+            public float inputMax = 1f;
+
+            public float outputMin = 0f;
+            public float outputMax = 1f;
+
+            public float output = 0f;
+
+            public void Step (float driver) {
+                output = Mathf.Lerp(outputMin, outputMax, Mathf.InverseLerp(inputMin, inputMax, driver));
+            }
+        }
+
+        [Range(0f,1f)]
+        public float driver = 0f;
+
+        public List<Key> keys = new List<Key>();
+
+        void Update()
+        {
+            foreach (var key in keys) {
+                key.Step(driver);
+            }
+        }
+    }
+}
