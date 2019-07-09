fbbabccd72ef1891a63b84f12d53cff42056182c
Jacob Bijani <jacob.bijani@gmail.com>
2019-07-09 16:58:57 -0400

    WristFollower

---
+++ Assets/_Scripts/WristFollower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace MrPuppet
+{
+    public class WristFollower : MonoBehaviour
+    {
+        [Required]
+        public MrPuppetDataMapper DataMapper;
+
+        private void OnValidate()
+        {
+            if (!DataMapper) DataMapper = FindObjectOfType<MrPuppetDataMapper>();
+        }
+
+        private void Update()
+        {
+            // TODO: convert to JointFollower and make this dynamic
+            transform.localPosition = DataMapper.WristJoint.position;
+            transform.localRotation = DataMapper.WristJoint.rotation;
+        }
+    }
+}
