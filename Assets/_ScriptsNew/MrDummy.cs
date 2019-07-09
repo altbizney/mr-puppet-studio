7dc0afe11258f110f0150fd9b1bc476bc9ea63d3
Luís Fonseca <luispedrofonseca@gmail.com>
2019-07-09 17:51:06 +0100

    Mr Dummy - Using colored cubes. Added control of the arm length of Mr. Dummy

--- Assets/_ScriptsNew/MrDummy.cs
+++ Assets/_ScriptsNew/MrDummy.cs
@@ -3,20 +3,59 @@ using UnityEngine;
 
 namespace Thinko.MrPuppet
 {
+    [ExecuteInEditMode]
     public class MrDummy : MonoBehaviour
     {
         [Required]
         public MrPuppetDataMapper DataMapper;
-
+        [Required]
         public Transform ShoulderJoint;
+        [Required]
+        public Transform ShoulderGeo;
+        [Required]
         public Transform ElbowJoint;
+        [Required]
+        public Transform ElbowGeo;
+        [Required]
         public Transform WristJoint;
-        
-        public Transform Jaw;
+        [Required]
+        public Transform JawJoint;
         
         private void Reset()
         {
             DataMapper = FindObjectOfType<MrPuppetDataMapper>();
+        }
+        
+        private void Update()
+        {
+            // Apply arm length
+            var armLength = ShoulderGeo.localScale;
+            armLength.x = DataMapper.ArmLength;
+            ShoulderGeo.localScale = armLength;
+            
+            var forearmLength = ElbowGeo.localScale;
+            forearmLength.x = DataMapper.ForearmLength;
+            ElbowGeo.localScale = forearmLength;
+            
+            // Adjust joints positions
+            var elbowPos = ElbowJoint.localPosition;
+            elbowPos.x = DataMapper.ArmLength;
+            ElbowJoint.localPosition = elbowPos;
+
+            var wristPos = WristJoint.localPosition;
+            wristPos.x = DataMapper.ForearmLength;
+            WristJoint.localPosition = wristPos;
+            
+            // We don't want to move the joints and jaw at edit time
+            if(!Application.isPlaying) return;
+            
+            // Rotate the joints
+            ShoulderJoint.rotation = DataMapper.FinalPose.ShoulderRotation;
+            ElbowJoint.rotation = DataMapper.FinalPose.ElbowRotation;
+            WristJoint.rotation = DataMapper.FinalPose.WristRotation;
+            
+            // Apply jaw movement
+            JawJoint.localEulerAngles = Vector3.forward * Mathf.LerpAngle(0f, -45.0f, DataMapper.JawPercent);
         }
     }
 }
