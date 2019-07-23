e5890daf8bb3287135ded7c484d99efe0b25a185
Jacob Bijani <jacob.bijani@gmail.com>
2019-07-23 12:38:43 -0700

    quick turntable script to rotate set

---
+++ Assets/_Scripts/Turntable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace MrPuppet {
+    public class Turntable : MonoBehaviour {
+        private float SmallAmount = 0.5f;
+        private float BigAmount = 1f;
+
+        private void Update() {
+            if (Input.GetKey("left")) {
+                transform.Rotate(0f, (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? BigAmount : SmallAmount, 0f);
+            }
+
+            if (Input.GetKey("right")) {
+                transform.Rotate(0f, (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? -BigAmount : -SmallAmount, 0f);
+            }
+        }
+    }
+}
