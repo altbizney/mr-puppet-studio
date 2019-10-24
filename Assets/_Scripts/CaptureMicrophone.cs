c92d21f875ed1e29b4b07603b24f505a3ddb94bd
Jacob Bijani <jacob.bijani@gmail.com>
2019-10-24 18:52:54 -0400

    pass filename in mic socket commands

--- Assets/_Scripts/CaptureMicrophone.cs
+++ Assets/_Scripts/CaptureMicrophone.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -19,9 +20,32 @@ namespace MrPuppet
 
         private bool RecordingMicrophone = false;
 
+        private string Filename;
+
         private void OnValidate()
         {
             if (HubConnection == null) HubConnection = FindObjectOfType<MrPuppetHubConnection>();
+        }
+
+        private void GetFilename()
+        {
+            RecorderControllerSettings m_ControllerSettings = RecorderControllerSettings.LoadOrCreate(Application.dataPath + "/../Library/Recorder/recorder.pref");
+            RecorderController m_RecorderController = new RecorderController(m_ControllerSettings);
+
+            foreach (var recorder in m_ControllerSettings.recorderSettings)
+            {
+                if (!recorder.enabled) continue;
+
+                Filename = recorder.outputFile;
+
+                Filename = Filename.Replace("<Take>", recorder.take.ToString("000"));
+                Filename = Filename.Replace("<Scene>", SceneManager.GetActiveScene().name);
+
+                Filename = Filename.Substring(Filename.LastIndexOf('/') + 1);
+
+                // just need the first
+                return;
+            }
         }
 
         private void Update()
@@ -32,12 +56,14 @@ namespace MrPuppet
 
             if (IsRecording && !RecordingMicrophone)
             {
-                HubConnection.SendSocketMessage("COMMAND;START_MICROPHONE");
+                GetFilename();
+                HubConnection.SendSocketMessage("COMMAND;START_MICROPHONE;" + Filename);
                 RecordingMicrophone = true;
             }
             else if (RecordingMicrophone && !IsRecording)
             {
-                HubConnection.SendSocketMessage("COMMAND;STOP_MICROPHONE");
+                // DO NOTT requery filename here as the take has already been advanced
+                HubConnection.SendSocketMessage("COMMAND;STOP_MICROPHONE;" + Filename);
                 RecordingMicrophone = false;
             }
         }
