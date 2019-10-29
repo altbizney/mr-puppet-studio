95cbd3cb5c8728c8c3b7fd17eb2eb0504766c63f
Jacob Bijani <jacob.bijani@gmail.com>
2019-10-29 17:13:08 -0400

    add namespace

--- Assets/_Scripts/ForceController.cs
+++ Assets/_Scripts/ForceController.cs
@@ -2,45 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForceController : MonoBehaviour
+namespace MrPuppet
 {
-    public Rigidbody rb;
-    public float WalkSpeed = 5f;
-
-    public float BobAmplitude = 1f;
-    public float BobSpeed = 2f;
-
-    private Vector3 InputVelocity;
-
-    private float Bob = 0f;
-    private float BobStartTime = 0f;
-    private float BobWave = 0f;
-
-    private void Update()
+    public class ForceController : MonoBehaviour
     {
-        if (Input.GetAxis("Horizontal") != 0f)
+        public Rigidbody rb;
+        public float WalkSpeed = 5f;
+
+        public float BobAmplitude = 1f;
+        public float BobSpeed = 2f;
+
+        private Vector3 InputVelocity;
+
+        private float Bob = 0f;
+        private float BobStartTime = 0f;
+        private float BobWave = 0f;
+
+        private void Update()
         {
-            if (BobStartTime == 0f) BobStartTime = Time.time;
+            if (Input.GetAxis("Horizontal") != 0f)
+            {
+                if (BobStartTime == 0f) BobStartTime = Time.time;
 
-            // moving
-            BobWave = Mathf.Sin((Time.time - BobStartTime) * BobSpeed);
-            Bob = Mathf.LerpUnclamped(0f, BobAmplitude, Input.GetAxis("Horizontal")) * BobWave;
+                // moving
+                BobWave = Mathf.Sin((Time.time - BobStartTime) * BobSpeed);
+                Bob = Mathf.LerpUnclamped(0f, BobAmplitude, Input.GetAxis("Horizontal")) * BobWave;
+            }
+            else
+            {
+                // resting
+                BobWave = BobStartTime = 0f;
+            }
+
+            InputVelocity = new Vector3(Input.GetAxis("Horizontal") * WalkSpeed, Input.GetAxis("Horizontal") == 0f ? rb.velocity.y : Bob, rb.velocity.z);
+
+            DebugGraph.Log("BobWave", BobWave);
+            DebugGraph.Log("Bob", Bob);
+            DebugGraph.Log("InputVelocity", InputVelocity);
         }
-        else
+
+        private void FixedUpdate()
         {
-            // resting
-            BobWave = BobStartTime = 0f;
+            rb.velocity = InputVelocity;
         }
-
-        InputVelocity = new Vector3(Input.GetAxis("Horizontal") * WalkSpeed, Input.GetAxis("Horizontal") == 0f ? rb.velocity.y : Bob, rb.velocity.z);
-
-        DebugGraph.Log("BobWave", BobWave);
-        DebugGraph.Log("Bob", Bob);
-        DebugGraph.Log("InputVelocity", InputVelocity);
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = InputVelocity;
     }
 }
