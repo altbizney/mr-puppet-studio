4843dd6b057031d897b46268b9c0c0d39a319ec9
Jacob Bijani <jacob.bijani@gmail.com>
2019-10-29 17:44:12 -0400

    tweak scene

--- Assets/_Scripts/ForceController.cs
+++ Assets/_Scripts/ForceController.cs
@@ -7,10 +7,10 @@ namespace MrPuppet
     public class ForceController : MonoBehaviour
     {
         public Rigidbody rb;
-        public float WalkSpeed = 5f;
+        public float WalkSpeed = 3.41f;
 
-        public float BobAmplitude = 1f;
-        public float BobSpeed = 2f;
+        public float BobAmplitude = 3.4f;
+        public float BobSpeed = 15f;
 
         private Vector3 InputVelocity;
 
