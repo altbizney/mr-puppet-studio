ed80d7a34792a74b3f1e1a35f7074e8a85190b4d
Jacob Bijani <jacob.bijani@gmail.com>
2019-10-29 14:44:00 -0400

    physics controller test

---
+++ Assets/_Scripts/ForceController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceController : MonoBehaviour
+{
+    public float WalkSpeed = 25f;
+    public float BobSpeed = 1f;
+
+    public float MaxSpeed = 20f;
+
+    public Rigidbody rb;
+
+    public KeyCode LeftKey = KeyCode.LeftArrow;
+    public KeyCode RightKey = KeyCode.RightArrow;
+    public KeyCode UpKey = KeyCode.UpArrow;
+
+    public ForceMode WalkMode = ForceMode.Force;
+    public ForceMode BobMode = ForceMode.Impulse;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(LeftKey))
+        {
+            rb.AddForce(-WalkSpeed, 0f, 0f, WalkMode);
+        }
+
+        if (Input.GetKey(RightKey))
+        {
+            rb.AddForce(WalkSpeed, 0f, 0f, WalkMode);
+        }
+
+        if (Input.GetKey(UpKey))
+        {
+            rb.AddForce(0f, BobSpeed, 0f, BobMode);
+        }
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxSpeed);
+    }
+}
