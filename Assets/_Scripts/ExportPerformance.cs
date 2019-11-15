24cf2c33cff144ed3a298688db3c0a5fcf2b9e5d
Jacob Bijani <jacob.bijani@gmail.com>
2019-11-15 17:48:01 -0500

    prep for buidling

--- Assets/_Scripts/ExportPerformance.cs
+++ Assets/_Scripts/ExportPerformance.cs
@@ -1,55 +1,67 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEditor.Formats.Fbx.Exporter;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
+#endif
 
-public class ExportPerformance : OdinEditorWindow
+namespace MrPuppet
 {
-    [MenuItem("Tools/Export Performances")]
-    private static void OpenWindow()
+#if UNITY_EDITOR
+    public class ExportPerformance : OdinEditorWindow
     {
-        GetWindow<ExportPerformance>().Show();
-    }
-
-    public GameObject Prefab;
-
-    public List<AnimationClip> Clips;
-
-    [Button(ButtonSizes.Large)]
-    private void Export()
-    {
-        int success = 0;
-
-        foreach (var clip in Clips)
+        [MenuItem("Tools/Export Performances")]
+        private static void OpenWindow()
         {
-            // create controller
-            var controller = AnimatorController.CreateAnimatorControllerAtPathWithClip("Assets/Recordings/" + clip.name + ".controller", clip);
-
-            // create instance, rename
-            var instance = GameObject.Instantiate(Prefab, Vector3.zero, Quaternion.identity);
-            instance.name = clip.name;
-
-            // add animator to instance
-            var animator = instance.AddComponent<Animator>();
-            animator.runtimeAnimatorController = controller;
-
-            // export
-            ModelExporter.ExportObject("Performances/" + clip.name + ".fbx", instance);
-
-            // cleanup
-            DestroyImmediate(instance);
-            AssetDatabase.DeleteAsset("Assets/Recordings/" + clip.name + ".controller");
-            FileUtil.MoveFileOrDirectory("Assets/Recordings/" + clip.name + ".anim", "Performances/" + clip.name + ".anim");
-
-            success++;
+            GetWindow<ExportPerformance>().Show();
         }
 
-        if (success == Clips.Count)
+        public GameObject Prefab;
+
+        public List<AnimationClip> Clips;
+
+        [Button(ButtonSizes.Large)]
+        private void Export()
         {
-            Clips = new List<AnimationClip>();
+            int success = 0;
+
+            foreach (var clip in Clips)
+            {
+                // create controller
+                var controller = AnimatorController.CreateAnimatorControllerAtPathWithClip("Assets/Recordings/" + clip.name + ".controller", clip);
+
+                // create instance, rename
+                var instance = GameObject.Instantiate(Prefab, Vector3.zero, Quaternion.identity);
+                instance.name = clip.name;
+
+                // add animator to instance
+                var animator = instance.AddComponent<Animator>();
+                animator.runtimeAnimatorController = controller;
+
+                // export
+                ModelExporter.ExportObject("Performances/" + clip.name + ".fbx", instance);
+
+                // cleanup
+                DestroyImmediate(instance);
+                AssetDatabase.DeleteAsset("Assets/Recordings/" + clip.name + ".controller");
+                FileUtil.MoveFileOrDirectory("Assets/Recordings/" + clip.name + ".anim", "Performances/" + clip.name + ".anim");
+
+                success++;
+            }
+
+            if (success == Clips.Count)
+            {
+                Clips = new List<AnimationClip>();
+            }
         }
     }
+#else
+public class ExportPerformance : MonoBehaviour {
+    
+}
+#endif
 }
