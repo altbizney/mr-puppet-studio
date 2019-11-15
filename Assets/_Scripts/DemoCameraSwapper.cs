2cf55d5b47c560b44330f121e945807de8aab4c9
Jacob Bijani <jacob.bijani@gmail.com>
2019-11-15 17:25:51 -0500

    camera swapper, adjust cameras

---
+++ Assets/_Scripts/DemoCameraSwapper.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MrPuppet
+{
+    public class DemoCameraSwapper : MonoBehaviour
+    {
+        public GameObject HeroCamera;
+        public GameObject PIPCamera;
+        public GameObject WideCamera;
+
+        public GameObject PIP;
+
+        public KeyCode HeroKey = KeyCode.Alpha1;
+        public KeyCode PIPKey = KeyCode.Alpha2;
+        public KeyCode WideKey = KeyCode.Alpha3;
+
+        private void Start()
+        {
+            HeroCamera.SetActive(true);
+            PIPCamera.SetActive(false);
+            WideCamera.SetActive(false);
+            PIP.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(HeroKey))
+            {
+                HeroCamera.SetActive(true);
+                PIPCamera.SetActive(false);
+                WideCamera.SetActive(false);
+                PIP.SetActive(false);
+            }
+
+            if (Input.GetKeyDown(PIPKey))
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+                PIP.SetActive(true);
+            }
+
+            if (Input.GetKeyDown(WideKey))
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(false);
+                WideCamera.SetActive(true);
+                PIP.SetActive(false);
+            }
+        }
+    }
+}
