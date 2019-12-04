c70b1f9626df5b4682f9583445920bce0dec7c7f
Jacob Bijani <jacob.bijani@gmail.com>
2019-12-04 17:05:09 -0500

    slide around, needs lookat

--- Assets/_Scripts/LocomotionController.cs
+++ Assets/_Scripts/LocomotionController.cs
@@ -1,29 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace MrPuppet.WIP
 {
     public class LocomotionController : MonoBehaviour
     {
-        private CharacterController controller;
+        private ValueDropdownList<Vector3> VectorDirectionValues = new ValueDropdownList<Vector3>()
+        {
+            {"Forward", Vector3.forward },
+            {"Back",    Vector3.back    },
+            {"Up",      Vector3.up      },
+            {"Down",    Vector3.down    },
+            {"Right",   Vector3.right   },
+            {"Left",    Vector3.left    },
+        };
+
+        public CharacterController controller;
+
+        [ValueDropdown("VectorDirectionValues", HideChildProperties = true)]
+        public Vector3 VerticalDirection = Vector3.forward;
+
+        [ValueDropdown("VectorDirectionValues", HideChildProperties = true)]
+        public Vector3 HorizontalDirection = Vector3.right;
 
         public float speed = 5f;
 
         private void Start()
         {
-            controller = GetComponent<CharacterController>();
+            if (!controller) controller = GetComponent<CharacterController>();
         }
 
         private void Update()
         {
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            Vector3 movementDirection = (VerticalDirection * Input.GetAxis("Vertical") * speed) + (HorizontalDirection * Input.GetAxis("Horizontal") * speed);
 
-            controller.Move(move * Time.deltaTime * speed);
-
-            if (move != Vector3.zero) transform.forward = move;
+            controller.Move(movementDirection * Time.deltaTime);
         }
-
-
     }
 }
