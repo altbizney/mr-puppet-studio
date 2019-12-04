a5bb159bf0e6a1f28d3350daaefb6d5540dd5a58
Jacob Bijani <jacob.bijani@gmail.com>
2019-12-04 17:38:55 -0500

    re-integrate bob

--- Assets/_Scripts/LocomotionController.cs
+++ Assets/_Scripts/LocomotionController.cs
@@ -17,7 +17,7 @@ namespace MrPuppet.WIP
             {"Left",    Vector3.left    },
         };
 
-        public CharacterController controller;
+        public CharacterController Controller;
 
         [ValueDropdown("VectorDirectionValues", HideChildProperties = true)]
         public Vector3 VerticalDirection = Vector3.forward;
@@ -25,18 +25,57 @@ namespace MrPuppet.WIP
         [ValueDropdown("VectorDirectionValues", HideChildProperties = true)]
         public Vector3 HorizontalDirection = Vector3.right;
 
-        public float speed = 5f;
+        public float WalkSpeed = 5f;
+
+        public float BobSmoothTime = 0.1f;
+        public float BobFrequency = 0.175f;
+        public float BobHeight = 2f;
+
+        private bool IsMoving, IsUp = false;
+        private float InitialHeight, BobTarget, BobCurrent, BobVelocity = 0f;
 
         private void Start()
         {
-            if (!controller) controller = GetComponent<CharacterController>();
+            if (!Controller) Controller = GetComponent<CharacterController>();
+
+            InitialHeight = transform.localPosition.y;
         }
 
         private void Update()
         {
-            Vector3 movementDirection = (VerticalDirection * Input.GetAxis("Vertical") * speed) + (HorizontalDirection * Input.GetAxis("Horizontal") * speed);
+            if (IsMoving)
+            {
+                // Reset bob when joysticks are released
+                if (Input.GetAxisRaw("Horizontal") == 0f && Input.GetAxisRaw("Vertical") == 0f)
+                {
+                    BobTarget = 0f;
+                    IsMoving = IsUp = false;
+                    CancelInvoke("ToggleBob");
+                }
+            }
+            else
+            {
+                // initiate bob as soon as joysticks are touched
+                if (Input.GetAxisRaw("Horizontal") != 0f || Input.GetAxisRaw("Vertical") != 0f)
+                {
+                    IsMoving = true;
+                    if (!IsInvoking("ToggleBob")) InvokeRepeating("ToggleBob", 0f, BobFrequency);
+                }
+            }
 
-            controller.Move(movementDirection * Time.deltaTime);
+            // create direction vector to move on X/Z
+            Vector3 movementDirection = (VerticalDirection * Input.GetAxis("Vertical") * WalkSpeed) + (HorizontalDirection * Input.GetAxis("Horizontal") * WalkSpeed);
+            Controller.Move(movementDirection * Time.deltaTime);
+
+            // spring bob height and apply to local position
+            BobCurrent = Mathf.SmoothDamp(BobCurrent, BobTarget, ref BobVelocity, BobSmoothTime);
+            transform.localPosition = new Vector3(transform.localPosition.x, InitialHeight + BobCurrent, transform.localPosition.z);
+        }
+
+        private void ToggleBob()
+        {
+            IsUp = !IsUp;
+            BobTarget = IsUp ? BobHeight : 0f;
         }
     }
 }
