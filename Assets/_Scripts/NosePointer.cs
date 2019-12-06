9b6cbd7915358b2cd49f8e10cb397858d37d7de6
Jacob Bijani <jacob.bijani@gmail.com>
2019-12-06 14:39:01 -0500

    move from WIP

--- Assets/_Scripts/NosePointer.cs
+++ Assets/_Scripts/NosePointer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
-namespace MrPuppet.WIP
+namespace MrPuppet
 {
     public class NosePointer : MonoBehaviour
     {
