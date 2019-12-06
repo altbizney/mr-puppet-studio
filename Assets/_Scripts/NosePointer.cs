249b5c881cb3b8f0f7fad2facf693b91e4e4bde9
Jacob Bijani <jacob.bijani@gmail.com>
2019-12-06 16:40:07 -0500

    cleanup

--- Assets/_Scripts/NosePointer.cs
+++ Assets/_Scripts/NosePointer.cs
@@ -28,6 +28,8 @@ namespace MrPuppet
         private float knobVelocity;
         private float angleFromCenter;
 
+        public bool EnableDebugGraph = false;
+
         private void OnDrawGizmos()
         {
             if (!Application.isPlaying) return;
@@ -63,68 +65,24 @@ namespace MrPuppet
             float hotspotAngle = hotspotSize / 2f;
             float hotspotKnob = Mathf.InverseLerp(0f, angleCutoff, hotspotAngle);
 
-            DebugGraph.MultiLog(angleFromCenter, "angleFromCenter");
-            DebugGraph.MultiLog(hotspotAngle, "hotspotAngle");
-            DebugGraph.MultiLog(-hotspotAngle, "-hotspotAngle");
-            DebugGraph.MultiLog(angleCutoff, "angleCutoff");
-            DebugGraph.MultiLog(-angleCutoff, "-angleCutoff");
-
+            // compute knob as absolute value
             knobTarget = Mathf.Abs(angleFromCenter).RemapAndClamp(0f, angleCutoff, -1f, 0f);
             if (Mathf.Abs(angleFromCenter) < hotspotAngle) knobTarget = hotspotKnob - 1f;
 
+            // match original sign
             if (angleFromCenter < 0f) knobTarget *= -1;
 
+            // TODO: only allow swap between hotspotKnob when "changing direction"
 
-            // if (angleFromCenter > 0f)
-            // {
-            //     knobTarget = angleFromCenter.RemapAndClamp(0f, angleCutoff, -1f, 0f);
-            //     if (angleFromCenter < hotspotAngle) knobTarget = hotspotKnob - 1f;
-            // }
-            // else
-            // {
-            //     knobTarget = angleFromCenter.RemapAndClamp(0f, -angleFromCenter, 0f, -1f);
-            //     if (angleFromCenter > -hotspotAngle) knobTarget = 1f - hotspotKnob;
-            // }
-
-            DebugGraph.Log(knobTarget);
-
-            /*
-            // remap from range to knobTarget range, then invert
-            knobTarget = angleFromCenter.Remap(-angleCutoff, angleCutoff, -1f, 1f) * -1;
-
-            // override hotspot and cutoff
-            if (angleFromCenter > 0f)
+            if (EnableDebugGraph)
             {
-                if (angleFromCenter < hotspotSize / 2f)
-                {
-                    // angleFromCenter = hotspotSize / 2f;
-                    // Debug.Log("HOTSPOT - RIGHT");
-                }
-
-                if (angleFromCenter > angleCutoff)
-                {
-                    // Debug.Log("CUTOFF - RIGHT");
-                    // angleFromCenter = 0f;
-                    knobTarget = 0f;
-                }
+                DebugGraph.MultiLog(angleFromCenter, "angleFromCenter");
+                DebugGraph.MultiLog(hotspotAngle, "hotspotAngle");
+                DebugGraph.MultiLog(-hotspotAngle, "-hotspotAngle");
+                DebugGraph.MultiLog(angleCutoff, "angleCutoff");
+                DebugGraph.MultiLog(-angleCutoff, "-angleCutoff");
+                DebugGraph.Log(knobTarget);
             }
-
-            if (angleFromCenter < 0f)
-            {
-                if (angleFromCenter > -hotspotSize / 2f)
-                {
-                    // angleFromCenter = -hotspotSize / 2f;
-                    // Debug.Log("HOTSPOT - LEFT");
-                }
-
-                if (angleFromCenter < -angleCutoff)
-                {
-                    // Debug.Log("CUTOFF - LEFT");
-                    // angleFromCenter = 0f;
-                    knobTarget = 0f;
-                }
-            }
-            */
 
             // spring the nose flop based on knobTarget
             knobCurr = Mathf.SmoothDamp(knobCurr, knobTarget, ref knobVelocity, knobSmoothTime);
@@ -135,30 +93,6 @@ namespace MrPuppet
         private void OnDisable()
         {
             noseJoint.localRotation = Quaternion.identity;
-        }
-
-        /// <summary>
-        /// Find some projected angle measure off some forward around some axis.
-        /// </summary>
-        /// <param name="v"></param>
-        /// <param name="forward"></param>
-        /// <param name="axis"></param>
-        /// <returns>Angle in degrees</returns>
-        // https://github.com/lordofduct/spacepuppy-unity-framework-3.0/blob/master/SpacepuppyUnityFramework/Utils/VectorUtil.cs#L297
-        public static float AngleOffAroundAxis(Vector3 v, Vector3 forward, Vector3 axis, bool clockwise = false)
-        {
-            Vector3 right;
-            if (clockwise)
-            {
-                right = Vector3.Cross(forward, axis);
-                forward = Vector3.Cross(axis, right);
-            }
-            else
-            {
-                right = Vector3.Cross(axis, forward);
-                forward = Vector3.Cross(right, axis);
-            }
-            return Mathf.Atan2(Vector3.Dot(v, right), Vector3.Dot(v, forward)) * Mathf.Rad2Deg;
         }
     }
 }
