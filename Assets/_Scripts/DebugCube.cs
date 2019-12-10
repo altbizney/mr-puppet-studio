e8518985515d1d6beb301c2d686cdfd0ea12d6bd
Jacob Bijani <jacob.bijani@gmail.com>
2019-12-10 15:31:34 -0500

    dumb script

---
+++ Assets/_Scripts/DebugCube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace MrPuppet
+{
+    public class DebugCube : MonoBehaviour
+    {
+        private void Start()
+        {
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            Vector3[] normals = mesh.normals;
+            Color[] colors = new Color[vertices.Length];
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                colors[i] = new Color(Mathf.Abs(normals[i].x), Mathf.Abs(normals[i].y), Mathf.Abs(normals[i].z));
+
+                if (normals[i].x < 0f) colors[i] -= new Color(0.6f, 0f, 0f);
+                if (normals[i].y < 0f) colors[i] -= new Color(0f, 0.6f, 0f);
+                if (normals[i].z < 0f) colors[i] -= new Color(0f, 0f, 0.6f);
+            }
+
+            mesh.colors = colors;
+        }
+    }
+}
