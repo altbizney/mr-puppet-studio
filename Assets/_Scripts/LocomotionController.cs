34f7a138987add7cbefcd7c57014936e841110a8
Jacob Bijani <jacob.bijani@gmail.com>
2019-12-17 16:26:28 -0500

    lint

--- Assets/_Scripts/LocomotionController.cs
+++ Assets/_Scripts/LocomotionController.cs
@@ -6,13 +6,7 @@ namespace MrPuppet.WIP
     public class LocomotionController : MonoBehaviour
     {
         private ValueDropdownList<Vector3> VectorDirectionValues = new ValueDropdownList<Vector3>()
-        {
-            {"Forward", Vector3.forward },
-            {"Back",    Vector3.back    },
-            {"Up",      Vector3.up      },
-            {"Down",    Vector3.down    },
-            {"Right",   Vector3.right   },
-            {"Left",    Vector3.left    },
+        { { "Forward", Vector3.forward }, { "Back", Vector3.back }, { "Up", Vector3.up }, { "Down", Vector3.down }, { "Right", Vector3.right }, { "Left", Vector3.left },
         };
 
         public CharacterController Controller;
