9d44bb9398e6dd01aa7d39f68dd2d6dc673a1c92
Jacob Bijani <jacob.bijani@gmail.com>
2020-01-27 13:05:58 -0500

    refactor pip swap to support next/prev

--- Assets/_Scripts/DemoCameraSwapper.cs
+++ Assets/_Scripts/DemoCameraSwapper.cs
@@ -18,106 +18,44 @@ namespace MrPuppet
         public GameObject PIP7;
         public GameObject PIP8;
 
+        public KeyCode PrevView = KeyCode.UpArrow;
+        public KeyCode NextView = KeyCode.DownArrow;
+        private int CurrentView = 1;
+
         private void Start()
         {
-            HeroCamera.SetActive(true);
-            PIPCamera.SetActive(false);
-            WideCamera.SetActive(false);
-
-            if (PIP1) PIP1.SetActive(false);
-            if (PIP2) PIP2.SetActive(false);
-            if (PIP3) PIP3.SetActive(false);
-            if (PIP4) PIP4.SetActive(false);
-            if (PIP5) PIP5.SetActive(false);
-            if (PIP6) PIP6.SetActive(false);
-            if (PIP7) PIP7.SetActive(false);
-            if (PIP8) PIP8.SetActive(false);
+            SwapGraphics(1);
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
+            if (Input.GetKeyDown(KeyCode.Alpha1)) SwapGraphics(1);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) SwapGraphics(2);
+            if (Input.GetKeyDown(KeyCode.Alpha3)) SwapGraphics(3);
+            if (Input.GetKeyDown(KeyCode.Alpha4)) SwapGraphics(4);
+            if (Input.GetKeyDown(KeyCode.Alpha5)) SwapGraphics(5);
+            if (Input.GetKeyDown(KeyCode.Alpha6)) SwapGraphics(6);
+            if (Input.GetKeyDown(KeyCode.Alpha7)) SwapGraphics(7);
+            if (Input.GetKeyDown(KeyCode.Alpha8)) SwapGraphics(8);
+            if (Input.GetKeyDown(KeyCode.Alpha9)) SwapGraphics(9);
+            if (Input.GetKeyDown(KeyCode.Alpha0)) SwapGraphics(0);
+
+            if (Input.GetKeyDown(PrevView))
             {
-                HeroCamera.SetActive(true);
-                PIPCamera.SetActive(false);
-                WideCamera.SetActive(false);
-                SwapPIP(null);
+                if (CurrentView == 0) CurrentView = 9;
+                else if (CurrentView != 1) CurrentView -= 1;
+                SwapGraphics(CurrentView);
             }
 
-            if (Input.GetKeyDown(KeyCode.Alpha2))
+            if (Input.GetKeyDown(NextView))
             {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP1);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP2);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha4))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP3);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha5))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP4);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha6))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP5);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha7))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP6);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha8))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP7);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha9))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(true);
-                WideCamera.SetActive(false);
-                SwapPIP(PIP8);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha0))
-            {
-                HeroCamera.SetActive(false);
-                PIPCamera.SetActive(false);
-                WideCamera.SetActive(true);
-                SwapPIP(null);
+                if (CurrentView == 9) CurrentView = 0;
+                else if (CurrentView != 0) CurrentView += 1;
+                SwapGraphics(CurrentView);
             }
         }
 
-        private void SwapPIP(GameObject active = null)
+        private void SwapGraphics(int view)
         {
             if (PIP1) PIP1.SetActive(false);
             if (PIP2) PIP2.SetActive(false);
@@ -128,7 +66,107 @@ namespace MrPuppet
             if (PIP7) PIP7.SetActive(false);
             if (PIP8) PIP8.SetActive(false);
 
-            if (active) active.SetActive(true);
+            if (view == 0)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(false);
+                WideCamera.SetActive(true);
+
+                CurrentView = 0;
+            }
+
+            if (view == 1)
+            {
+                HeroCamera.SetActive(true);
+                PIPCamera.SetActive(false);
+                WideCamera.SetActive(false);
+
+                CurrentView = 1;
+            }
+
+            if (view == 2)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+
+                if (PIP1) PIP1.SetActive(true);
+
+                CurrentView = 2;
+            }
+
+            if (view == 3)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+
+                if (PIP2) PIP2.SetActive(true);
+
+                CurrentView = 3;
+            }
+
+            if (view == 4)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+
+                if (PIP3) PIP3.SetActive(true);
+
+                CurrentView = 4;
+            }
+
+            if (view == 5)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+
+                if (PIP4) PIP4.SetActive(true);
+
+                CurrentView = 5;
+            }
+
+            if (view == 6)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+
+                if (PIP5) PIP5.SetActive(true);
+
+                CurrentView = 6;
+            }
+
+            if (view == 7)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+
+                if (PIP6) PIP6.SetActive(true);
+
+                CurrentView = 7;
+            }
+
+            if (view == 8)
+            {
+                HeroCamera.SetActive(false);
+                PIPCamera.SetActive(true);
+                WideCamera.SetActive(false);
+
+                if (PIP7) PIP7.SetActive(true);
+
+                CurrentView = 8;
+            }
+
+            if (view == 9)
+            {
+                if (PIP8) PIP8.SetActive(true);
+
+                CurrentView = 9;
+            }
         }
     }
 }
