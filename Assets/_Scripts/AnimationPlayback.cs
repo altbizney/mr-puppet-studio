476551ebc793df14f28f6e75a03b0a4b116c7481
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-03-09 14:46:55 -0400

    development version of Animation Playback

---
+++ Assets/_Scripts/AnimationPlayback.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlayback : MonoBehaviour
+{
+    AudioSource audSource;
+
+    Animator anim;
+    public AnimationClip animClip;
+    public AudioClip audClip;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.enabled = false;
+        animClip.legacy = false;
+    }
+
+    public void playAnim()
+    {
+        anim.enabled = true;
+
+        AnimatorOverrideController animatorOverrideController = new AnimatorOverrideController();
+        animatorOverrideController.runtimeAnimatorController = anim.runtimeAnimatorController;
+        animatorOverrideController["testAnim"] = animClip;
+        anim.runtimeAnimatorController = animatorOverrideController;
+
+        audSource = gameObject.AddComponent<AudioSource>();
+        audSource.clip = audClip;
+        audSource.Play();
+
+    }
+
+}
---
+++ Assets/_Scripts/customInspector.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AnimationPlayback))]
+public class customInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        AnimationPlayback ac = (AnimationPlayback)target;
+        if (GUILayout.Button("PLAY"))
+        {
+            ac.playAnim();
+        }
+
+    }
+
+}
