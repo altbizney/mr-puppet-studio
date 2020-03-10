793a32b1674f6833f26f978c25b4b251e252e988
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-03-10 18:53:55 -0400

    Editor window functionality, updated UI, runtime audio functionality, start of audio streaming

--- Assets/_Scripts/AnimationPlayback.cs
+++ Assets/_Scripts/AnimationPlayback.cs
@@ -1,6 +1,11 @@
 ﻿
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -12,22 +17,48 @@ using Sirenix.OdinInspector.Editor;
 
 namespace MrPuppet
 {
+
 #if UNITY_EDITOR
     public class AnimationPlayback : OdinEditorWindow
     {
+
+        //MonoBehaviour instance;
+
+        private GameObject clone;
+        private Renderer[] rs;
+        private AudioClip audClip;
+        private string tempController = "temp.controller";
+
+        public AnimationClip animClip;
+        public GameObject actor;
+
         [MenuItem("Tools/Animation Playback")]
         private static void OpenWindow()
         {
             GetWindow<AnimationPlayback>().Show();
         }
 
-        GameObject clone;
+        IEnumerator streamAudioClip()
+        {
+            Debug.Log("Calling Coroutine");
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("https://hypermesh.accelerator.net/renders/QNgToB/audio", AudioType.OGGVORBIS))
+            {
+                yield return www.SendWebRequest();
 
-        bool playing = false;
+                if (www.isNetworkError)
+                    Debug.Log(www.error);
+                else
+                    audClip = DownloadHandlerAudioClip.GetContent(www);
+            }
+        }
 
-        public AnimationClip animClip;
-        public AudioClip audClip;
-        public GameObject actor;
+        private void OnEnable()
+        {
+            //MonoBehaviour myMono = actor.GetComponent<MonoBehaviour>();
+            //myMono.StartCoroutine(streamAudioClip());
+
+            rs = actor.GetComponentsInChildren<Renderer>();
+        }
 
         [GUIColor(0.2f, 0.9f, 0.2f)]
         [ButtonGroup]
@@ -35,35 +66,32 @@ namespace MrPuppet
         [DisableInEditorMode]
         private void playAnim()
         {
-            if (playing == false)
+            if (!clone)
             {
                 clone = Instantiate(actor, actor.transform.position, Quaternion.identity);
 
-                Destroy(clone.GetComponent<MrPuppet.Blink>());
-                Destroy(clone.GetComponent<MrPuppet.JawTransformMapper>());
-                Destroy(clone.GetComponent<MrPuppet.ButtPuppet>());
+                Destroy(clone.GetComponent<Blink>());
+                Destroy(clone.GetComponent<JawTransformMapper>());
+                Destroy(clone.GetComponent<ButtPuppet>());
 
-                Animator cloneAnim = clone.GetComponent<Animator>();
+                var controller = AnimatorController.CreateAnimatorControllerAtPathWithClip("Assets/Recordings/"+tempController, animClip);
+
+                Animator cloneAnim = clone.AddComponent<Animator>();
 
                 cloneAnim.enabled = true;
 
-                AnimatorOverrideController animatorOverrideController = new AnimatorOverrideController();
-                animatorOverrideController.runtimeAnimatorController = cloneAnim.runtimeAnimatorController;
-                animatorOverrideController["testAnim"] = animClip;
-                cloneAnim.runtimeAnimatorController = animatorOverrideController;
+                cloneAnim.runtimeAnimatorController = controller;
 
                 AudioSource audSource = clone.AddComponent<AudioSource>();
                 audSource.clip = audClip;
                 audSource.Play();
 
-                Renderer[] rs = actor.GetComponentsInChildren<Renderer>();
                 foreach (Renderer r in rs)
                     r.enabled = false;
-
-                playing = true;
             }
 
         }
+
 
         [GUIColor(0.9f, 0.3f, 0.3f)]
         [ButtonGroup]
@@ -71,22 +99,35 @@ namespace MrPuppet
         [DisableInEditorMode]
         private void stopAnim()
         {
-            if (playing == true)
+           if (clone)
             {
-                Renderer[] rs = actor.GetComponentsInChildren<Renderer>();
                 foreach (Renderer r in rs)
                     r.enabled = true;
 
                 Destroy(clone);
-
-                playing = false;
+                AssetDatabase.DeleteAsset("Assets/Recordings/"+tempController);
             }
         }
-
     }
+
 #else
 public class AnimationPlayback : MonoBehaviour {
-    
+
 }
 #endif
+
+    [InitializeOnLoadAttribute]
+    public static class PlayModeStateChanged
+    {
+        static PlayModeStateChanged()
+        {
+            EditorApplication.playModeStateChanged += playModes;
+        }
+
+        private static void playModes(PlayModeStateChange state)
+        {
+            AssetDatabase.DeleteAsset("Assets/Recordings/temp.controller");
+        }
+    }
+
 }
