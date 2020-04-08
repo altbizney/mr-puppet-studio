caa69a612089db54e1d4622a1c2b54e30b8b3907
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-04-08 18:56:23 -0400

    First iteration of Audio Reference tool

---
+++ Assets/_Scripts/AudioReference.cs
@@ -0,0 +1,104 @@
+﻿
+#if UNITY_EDITOR
+using UnityEditor;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor.Recorder;
+#endif
+
+namespace MrPuppet
+{
+#if UNITY_EDITOR
+    public class AudioReference : OdinEditorWindow
+    {
+        [MenuItem("Tools/Audio Reference")]
+        private static void OpenWindow()
+        {
+            GetWindow<AudioReference>().Show();
+        }
+
+        private MrPuppetHubConnection HubConnection;
+        private RecorderWindow Recorder;
+        private bool AudioIsPlaying;
+        private string TakeAfterPlay;
+
+        public string Take;
+        public bool EnterPlayback;
+
+        private void Update()
+        {
+            if (EditorApplication.isPlaying)
+            {
+                if (Recorder)
+                {
+                    if (Recorder.IsRecording())
+                    {
+                        if (EnterPlayback == true)
+                        {
+                            if (AudioIsPlaying == false)
+                            {
+                                //bool found = false;
+                                //string DataPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments) + "/Downloads";
+                                //DirectoryInfo d = new DirectoryInfo(DataPath);
+                                //FileInfo[] Files = d.GetFiles("*.wav");
+                                //foreach (FileInfo file in Files)
+                                //{
+                                  //  if (file.Name == Take.ToUpper() + ".wav")
+                                  //       found = true;
+                                //}
+
+                                //if (found == true)
+                                //{
+                                    TakeAfterPlay = Take;
+                                    HubConnection.SendSocketMessage("COMMAND;PLAYBACK;START;" + TakeAfterPlay);
+                                    AudioIsPlaying = true;
+                                //}
+                                //else
+                                  //  Debug.Log("Could not find the audio file associated with Audio Reference Take");
+                            }
+                        }
+
+                    }
+
+                    if (!Recorder.IsRecording())
+                    {
+                        if (AudioIsPlaying == true)
+                        {
+                            HubConnection.SendSocketMessage("COMMAND;PLAYBACK;STOP;" + TakeAfterPlay);
+                            AudioIsPlaying = false;
+                        }
+                    }
+                }
+            }
+
+            if (!EditorApplication.isPlaying)
+            {
+                HubConnection.SendSocketMessage("COMMAND;PLAYBACK;STOP;" + TakeAfterPlay);
+
+                if (AudioIsPlaying == true)
+                    AudioIsPlaying = false;
+
+                TakeAfterPlay = "";
+            }
+
+            if(!Recorder)
+                Recorder = EditorWindow.GetWindow<RecorderWindow>();
+
+            if (!HubConnection)
+                HubConnection = FindObjectOfType<MrPuppetHubConnection>();
+        }
+
+
+        public void OnGUI()
+        {
+            base.OnGUI();
+            EditorGUILayout.HelpBox("Will play an audio file with the given filename in ~/Downloads in sync with recording. For example: to play FC_day1.aif in sync with recording, place the FC_day1.aif file in ~/Downloads, enter \"FC_day1\" as the filename, and start the node ableton link script.", MessageType.Warning);
+            Repaint();
+        }
+
+    }
+#else
+public class AnimationPlayback : MonoBehaviour {
+
+}
+#endif
+}
