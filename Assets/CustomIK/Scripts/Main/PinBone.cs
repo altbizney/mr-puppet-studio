9630f19de68f68a1456a3292717858fd1c0a215a
Jacob Bijani <jacob.bijani@gmail.com>
2020-04-09 15:38:34 -0400

    pin bone WIP. jitters a bit.

---
+++ Assets/CustomIK/Scripts/Main/PinBone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinBone : MonoBehaviour
+{
+    private Quaternion rot;
+    private Vector3 pos;
+
+    public Transform bone;
+
+    private void Awake()
+    {
+        rot = bone.rotation;
+        pos = bone.position;
+    }
+
+    private void Update() {
+        bone.rotation = rot;
+        bone.position = pos;
+    }
+}
