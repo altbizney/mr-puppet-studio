c29196169c5aedf0cb96375ed09bf4a429a010d9
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-05-28 16:01:47 -0400

    Quaternion angle axis test

--- Assets/_Scripts/PoseSpaceObjectTest.cs
+++ Assets/_Scripts/PoseSpaceObjectTest.cs
@@ -108,6 +108,10 @@ namespace MrPuppet
             ScoreZ2 = RemapAndClamp(gameObject.transform.localRotation.eulerAngles.z, DataMapper.AttachPose.ShoulderRotation.eulerAngles.z, Pose2.eulerAngles.z, 0f, 1f);
             ScoreTotal2 = ScoreY2 + ScoreX2 + ScoreZ2; //RemapAndClamp(ScoreTotal2, 0f, , 0f, 1f);
 
+            Quaternion ay = Quaternion.AngleAxis(Attach.eulerAngles.y, Vector3.up);
+            Quaternion py = Quaternion.AngleAxis(Pose1.eulerAngles.y, Vector3.up);
+            Debug.Log(Quaternion.Angle(ay, py));
+
             // DeltaClamped = EulerAnglesClamp(DeltaQuat);
             // DeltaEuler = DeltaQuat.eulerAngles;
         }
