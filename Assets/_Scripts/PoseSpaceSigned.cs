ca04ceba8ab0702311d684030ae063735fc347ad
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-06-04 11:27:08 -0400

    Up direction

--- Assets/_Scripts/PoseSpaceSigned.cs
+++ Assets/_Scripts/PoseSpaceSigned.cs
@@ -88,35 +88,43 @@ namespace MrPuppet
             gameObject.transform.localRotation = RotationDeltaFromAttachWrist();
 
             var liveForward = gameObject.transform.rotation * Vector3.forward;
+            var liveUp = gameObject.transform.rotation * Vector3.up;
             var zeroForward = Quaternion.identity * Vector3.forward;
+            var zeroUp = Quaternion.identity * Vector3.up;
+
             var poseNorthForward = poseNorth * Vector3.forward;
             var poseSouthForward = poseSouth * Vector3.forward;
             var poseEastForward = poseEast * Vector3.forward;
             var poseWestForward = poseWest * Vector3.forward;
 
+            var poseNorthUp = poseNorth * Vector3.up;
+            var poseSouthUp = poseSouth * Vector3.up;
+            var poseEastUp = poseEast * Vector3.up;
+            var poseWestUp = poseWest * Vector3.up;
+
             liveXZ = Mathf.Atan2(liveForward.x, liveForward.z) * Mathf.Rad2Deg;
-            liveXY = Mathf.Atan2(liveForward.x, liveForward.y) * Mathf.Rad2Deg;
-            liveYZ = Mathf.Atan2(liveForward.y, liveForward.z) * Mathf.Rad2Deg;
+            liveXY = Mathf.Atan2(liveUp.x, liveUp.y) * Mathf.Rad2Deg;
+            liveYZ = Mathf.Atan2(liveUp.y, liveUp.z) * Mathf.Rad2Deg;
 
             zeroXZ = Mathf.Atan2(zeroForward.y, zeroForward.z) * Mathf.Rad2Deg;
-            zeroXY = Mathf.Atan2(zeroForward.x, zeroForward.y) * Mathf.Rad2Deg;
-            zeroYZ = Mathf.Atan2(zeroForward.y, zeroForward.z) * Mathf.Rad2Deg;
+            zeroXY = Mathf.Atan2(zeroUp.x, zeroUp.y) * Mathf.Rad2Deg;
+            zeroYZ = Mathf.Atan2(zeroUp.y, zeroUp.z) * Mathf.Rad2Deg;
 
             poseNorthSeperated.x = Mathf.Atan2(poseNorthForward.x, poseNorthForward.z) * Mathf.Rad2Deg;
-            poseNorthSeperated.y = Mathf.Atan2(poseNorthForward.x, poseNorthForward.y) * Mathf.Rad2Deg;
-            poseNorthSeperated.z = Mathf.Atan2(poseNorthForward.y, poseNorthForward.z) * Mathf.Rad2Deg;
+            poseNorthSeperated.y = Mathf.Atan2(poseNorthUp.x, poseNorthUp.y) * Mathf.Rad2Deg;
+            poseNorthSeperated.z = Mathf.Atan2(poseNorthUp.y, poseNorthUp.z) * Mathf.Rad2Deg;
 
             poseSouthSeperated.x = Mathf.Atan2(poseSouthForward.x, poseSouthForward.z) * Mathf.Rad2Deg;
-            poseSouthSeperated.y = Mathf.Atan2(poseSouthForward.x, poseSouthForward.y) * Mathf.Rad2Deg;
-            poseSouthSeperated.z = Mathf.Atan2(poseSouthForward.y, poseSouthForward.z) * Mathf.Rad2Deg;
+            poseSouthSeperated.y = Mathf.Atan2(poseSouthUp.x, poseSouthUp.y) * Mathf.Rad2Deg;
+            poseSouthSeperated.z = Mathf.Atan2(poseSouthUp.y, poseSouthUp.z) * Mathf.Rad2Deg;
 
             poseEastSeperated.x = Mathf.Atan2(poseEastForward.x, poseEastForward.z) * Mathf.Rad2Deg;
-            poseEastSeperated.y = Mathf.Atan2(poseEastForward.x, poseEastForward.y) * Mathf.Rad2Deg;
-            poseEastSeperated.z = Mathf.Atan2(poseEastForward.y, poseEastForward.z) * Mathf.Rad2Deg;
+            poseEastSeperated.y = Mathf.Atan2(poseEastUp.x, poseEastUp.y) * Mathf.Rad2Deg;
+            poseEastSeperated.z = Mathf.Atan2(poseEastUp.y, poseEastUp.z) * Mathf.Rad2Deg;
 
             poseWestSeperated.x = Mathf.Atan2(poseWestForward.x, poseWestForward.z) * Mathf.Rad2Deg;
-            poseWestSeperated.y = Mathf.Atan2(poseWestForward.x, poseWestForward.y) * Mathf.Rad2Deg;
-            poseWestSeperated.z = Mathf.Atan2(poseWestForward.y, poseWestForward.z) * Mathf.Rad2Deg;
+            poseWestSeperated.y = Mathf.Atan2(poseWestUp.x, poseWestUp.y) * Mathf.Rad2Deg;
+            poseWestSeperated.z = Mathf.Atan2(poseWestUp.y, poseWestUp.z) * Mathf.Rad2Deg;
 
             deltaLiveOriginXZ = Mathf.DeltaAngle(zeroXZ, liveXZ);
             deltaLiveOriginXY = Mathf.DeltaAngle(zeroXY, liveXY);
