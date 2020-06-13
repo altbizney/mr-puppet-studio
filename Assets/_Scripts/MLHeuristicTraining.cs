6c2d9216fd625c926dfca610a140fbcb4402ce19
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-06-12 22:46:07 -0400

    2DTraining

--- Assets/_Scripts/MLHeuristicTraining.cs
+++ Assets/_Scripts/MLHeuristicTraining.cs
@@ -10,7 +10,6 @@ public class MLHeuristicTraining : Agent
     private float MyScore;
     private float RealScore;
 
-
     public override void Initialize()
     {
         //SetResetParameters();
@@ -18,24 +17,23 @@ public class MLHeuristicTraining : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        //send score
-        //send my pos and goal pose
-        //does goal pos just stay the same?
         sensor.AddObservation(gameObject.transform.position);
         sensor.AddObservation(Goal.transform.position);
 
-        //observe score somehow
         sensor.AddObservation(RealScore);
     }
 
 
     public override void OnActionReceived(float[] vectorAction)
     {
-        RealScore = Mathf.Abs(gameObject.transform.position.x - Goal.transform.position.x);
-        RealScore = Remap(RealScore, 0f, 6f, 0f, 1f);
+        Vector3 GoalVector = new Vector3(gameObject.transform.position.x, 0f, gameObject.transform.position.z);
+        Vector3 MyVector = new Vector3(Goal.transform.position.x, 0f, Goal.transform.position.z);
+        float MyReward;
+
+        RealScore = Vector3.Distance(GoalVector, MyVector);
+        RealScore = Remap(RealScore, 0f, 8.485281f, 0f, 1f);
 
         MyScore = vectorAction[0];
-        float MyReward;
 
         if (MyScore < 0)
         {
@@ -43,29 +41,24 @@ public class MLHeuristicTraining : Agent
             EndEpisode();
         }
 
-        if (Mathf.Abs(RealScore - MyScore) < 0.2f)
-        {
-            MyReward = 0.1f;//SetReward(0.3f);
-        }
+        if (RealScore == MyScore)
+            MyReward = 1f;
+        else if (Mathf.Abs(RealScore - MyScore) < 0.01f)
+            MyReward = 0.5f;
+        else if (Mathf.Abs(RealScore - MyScore) < 0.08f)
+            MyReward = 0.3f;
         else if (Mathf.Abs(RealScore - MyScore) < 0.1f)
-            MyReward = 0.3f; //SetReward(0.7f);
-        else if (Mathf.Abs(RealScore - MyScore) < 0.05f)
-            MyReward = 0.5f; //SetReward(0.7f);
-        else if (RealScore == MyScore)
-            MyReward = 1f; //SetReward(1f);
+            MyReward = 0.1f;
         else
-            MyReward = -0.2f; //SetReward(-0.2f);
-
+            MyReward = -0.2f;
 
         //DebugGraph.MultiLog("Scores ", Color.white, RealScore, "RealScore");
         //DebugGraph.MultiLog("Scores ", Color.green, MyScore, "MyScore");
         //DebugGraph.MultiLog("Scores ", Color.blue, MyReward, "Reward");
-        Debug.Log(RealScore + " " + MyScore);// + " " + MyReward);
+        //Debug.Log(RealScore + " " + MyScore);// + " " + MyReward);
 
         SetReward(MyReward);
         EndEpisode();
-
-        // How correct was your score? if 0==0 give 1, if 0.5==0.5 give 1. if 1==0 give 0;
     }
 
     private float Remap(float value, float from1, float to1, float from2, float to2)
@@ -77,7 +70,6 @@ public class MLHeuristicTraining : Agent
         public override void Heuristic(float[] actionsOut)
         {
             //put movement in here
-            //left right
 
             if (Input.GetKey(KeyCode.D))
             {
@@ -89,11 +81,12 @@ public class MLHeuristicTraining : Agent
                 transform.position += new Vector3(-3f * Time.deltaTime, 0, 0);
 
         }
-        */
+    */
 
     public override void OnEpisodeBegin()
     {
-        //random local of agent. or would that be in init?
-        transform.localPosition = new Vector3(Random.Range(-3f, 3f), gameObject.transform.localPosition.y, 0f);
+        //Should I be doing something inside of init?
+        transform.localPosition = new Vector3(Random.Range(-3f, 3f), gameObject.transform.localPosition.y, Random.Range(-3f, 3f));
+        Goal.transform.localPosition = new Vector3(Random.Range(-3f, 3f), Goal.transform.localPosition.y, Random.Range(-3f, 3f));
     }
 }
