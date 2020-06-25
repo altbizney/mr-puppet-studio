f5b0413c2e634869500ccca0c396506c667e1212
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-06-24 20:26:21 -0400

    Quardanant training

--- Assets/_Scripts/MachineLearning/MLHeuristicQuadrant.cs
+++ Assets/_Scripts/MachineLearning/MLHeuristicQuadrant.cs
@@ -6,7 +6,8 @@ using Unity.MLAgents.Sensors;
 
 public class MLHeuristicQuadrant : Agent
 {
-    public List<GameObject> TouchingQuadrants = new List<GameObject>();
+    private GameObject[] Quadrants;
+    private float IdealQuadrant;
 
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -14,8 +15,32 @@ public class MLHeuristicQuadrant : Agent
         sensor.AddObservation(gameObject.transform.position.z);
     }
 
+
+    public override void Initialize()
+    {
+        IdealQuadrant = 999999999;
+        Quadrants = new GameObject[4];
+        Quadrants[0] = GameObject.Find("QuadrantRightDown");
+        Quadrants[1] = GameObject.Find("QuadrantRightUp");
+        Quadrants[2] = GameObject.Find("QuadrantLeftDown");
+        Quadrants[3] = GameObject.Find("QuadrantLeftUp");
+    }
+
     public override void OnActionReceived(float[] vectorAction)
     {
+
+        IdealQuadrant = vectorAction[0];
+        Debug.Log(vectorAction[0]);
+
+        for (int i = 0; i < 4; i++)
+        {
+            if ((int)IdealQuadrant == i)
+                Quadrants[i].transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);//(Vector3.down * 180f * Time.deltaTime);
+            else
+                Quadrants[i].transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        /*
         if (TouchingQuadrants.Count > 1)
         {
             SetReward(-1f);
@@ -34,24 +59,31 @@ public class MLHeuristicQuadrant : Agent
             else
                 SetReward(-1f);
         }
-
+        */
     }
 
-    private void OnCollisionEnter(Collision Quadrant)
+    /*
+    public override void CollectDiscreteActionMasks(DiscreteActionMasker actionMasker)
     {
-        TouchingQuadrants.Add(Quadrant.gameObject);
-    }
+        if (IdealQuadrant == 0)
+            actionMasker.SetMask(0, new int[3] { 1, 2, 3 });
+        else if (IdealQuadrant == 1)
+            actionMasker.SetMask(0, new int[3] { 0, 2, 3 });
+        else if (IdealQuadrant == 2)
+            actionMasker.SetMask(0, new int[3] { 1, 0, 3 });
+        else if (IdealQuadrant == 3)
+            actionMasker.SetMask(0, new int[3] { 1, 2, 0 });
 
-    void OnCollisionExit(Collision Quadrant)
-    {
-        TouchingQuadrants.Remove(Quadrant.gameObject);
+        IdealQuadrant = 999999999;
     }
+    */
 
+    /*
     public override void Heuristic(float[] actionsOut)
     {
         //actionsOut[0] = 0;
 
-        if (Input.GetKey(KeyCode.I))
+        if (Input.GetKey(KeyCode.I))l
             actionsOut[0] = 1;
         if (Input.GetKey(KeyCode.O))
             actionsOut[0] = 2;
@@ -60,19 +92,40 @@ public class MLHeuristicQuadrant : Agent
         if (Input.GetKey(KeyCode.L))
             actionsOut[0] = 4;
     }
+    */
+
+    /*
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(0f, gameObject.transform.localPosition.y, 0f);
+        IdealQuadrant = 999999999;
+        foreach (GameObject q in Quadrants)
+        {
+            q.GetComponent<Renderer>().transform.localRotation = Quaternion.identity;
+        }
+    }
+    */
 
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.down * 90f * Time.deltaTime);
+            transform.position += (transform.right * 1f) * Time.deltaTime / 2f;
 
         if (Input.GetKey(KeyCode.D))
-            transform.Rotate(Vector3.up * 90f * Time.deltaTime);
+            transform.position += (transform.right * -1f) * Time.deltaTime / 2f;
 
         if (Input.GetKey(KeyCode.S))
-            transform.position += transform.forward * Time.deltaTime * 4f;
+            transform.position += transform.forward * Time.deltaTime / 2f;
 
         if (Input.GetKey(KeyCode.W))
-            transform.position += (transform.forward * -1f) * Time.deltaTime * 4f;
+            transform.position += (transform.forward * -1f) * Time.deltaTime / 2f;
+
+        if (Input.GetKey("space"))
+        {
+            SetReward(1f);
+            EndEpisode();
+        }
     }
+
+
 }
